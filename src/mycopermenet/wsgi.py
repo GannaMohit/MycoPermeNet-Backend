43a07499d324c4baeb8f381a89eb45f02fec6348
Mohit Ganna <mohitganna@gmail.com>
2025-04-26 04:45:15 -0400

    Initialized project

---
+++ src/mycopermenet/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mycopermenet project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycopermenet.settings')
+
+application = get_wsgi_application()
